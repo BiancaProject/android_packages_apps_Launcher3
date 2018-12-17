27010e87be2fe9dfe84d0d1d60e9c4d5a3e5e016
Chris <chris@cypheros.co>
2018-12-16 19:11:15 -0500

    Launcher3: Introduce Quickspace [squashed]
    Implements a date/time and weather view on the first page of the
    homescreen similar to Google's Smartspace.
    
    This completely replaces the legacy QSB with our view and uses an open
    source WeatherProvider client to retrieve weather information
    
    Bootleggers edit: Let's make it toggleable, by moving our showQSB into showQuickspace, using At a Glance strings.
    
    AICP Edit: adapted to OmniJaws weather provider
    
    includes
    * c004066d8 - Launcher3: quickspace fixes <Ali B>
    * 8d281abcb - QuickSpace: refactor weather integration  <Ali B>
    * 4572b7a7e - Quickspace: properly handle weather updates <Ali B>
    * 0769a31ac - Quickspace: Create action receivers for date/weather view <Chris>
    * d7fad74e6 - Launcher3: Introduce Quickspace <Chris>
    
    Change-Id: Iac9c8099007f790322342e23b5911fc2ecf808ea
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- src/com/android/launcher3/Launcher.java
+++ src/com/android/launcher3/Launcher.java
@@ -152,7 +152,7 @@ import com.android.launcher3.pm.UserCache;
 import com.android.launcher3.popup.PopupContainerWithArrow;
 import com.android.launcher3.popup.PopupDataProvider;
 import com.android.launcher3.popup.SystemShortcut;
-import com.android.launcher3.qsb.QsbContainerView;
+import com.android.launcher3.quickspace.QuickSpaceView;
 import com.android.launcher3.statemanager.StateManager;
 import com.android.launcher3.statemanager.StateManager.StateHandler;
 import com.android.launcher3.statemanager.StatefulActivity;
@@ -353,6 +353,8 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
     private boolean mTouchInProgress;
 
     private SafeCloseable mUserChangedCallbackCloseable;
+    // QuickSpace
+    private QuickSpaceView mQuickSpace;
 
     // New InstanceId is assigned to mAllAppsSessionLogId for each AllApps sessions.
     // When Launcher is not in AllApps state mAllAppsSessionLogId will be null.
@@ -477,6 +479,7 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         // For handling default keys
         setDefaultKeyMode(DEFAULT_KEYS_SEARCH_LOCAL);
 
+        mQuickSpace = findViewById(R.id.reserved_container_workspace);
         setContentView(getRootView());
         getRootView().dispatchInsets();
 
@@ -1098,6 +1101,10 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         Object traceToken = TraceHelper.INSTANCE.beginSection(ON_RESUME_EVT,
                 TraceHelper.FLAG_UI_EVENT);
         super.onResume();
+
+        if (mQuickSpace != null) {
+            mQuickSpace.onResume();
+        }
 
         if (mDeferOverlayCallbacks) {
             scheduleDeferredCheck();
@@ -2144,11 +2151,11 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
     @Override
     public void bindScreens(IntArray orderedScreenIds) {
         // Make sure the first screen is always at the start.
-        if (FeatureFlags.QSB_ON_FIRST_SCREEN &&
+        if (Utilities.showQuickspace(this) &&
                 orderedScreenIds.indexOf(Workspace.FIRST_SCREEN_ID) != 0) {
             orderedScreenIds.removeValue(Workspace.FIRST_SCREEN_ID);
             orderedScreenIds.add(0, Workspace.FIRST_SCREEN_ID);
-        } else if (!FeatureFlags.QSB_ON_FIRST_SCREEN && orderedScreenIds.isEmpty()) {
+        } else if (!Utilities.showQuickspace(this) && orderedScreenIds.isEmpty()) {
             // If there are no screens, we need to have an empty screen
             mWorkspace.addExtraEmptyScreen();
         }
@@ -2164,7 +2171,7 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
         int count = orderedScreenIds.size();
         for (int i = 0; i < count; i++) {
             int screenId = orderedScreenIds.get(i);
-            if (!FeatureFlags.QSB_ON_FIRST_SCREEN || screenId != Workspace.FIRST_SCREEN_ID) {
+            if (!Utilities.showQuickspace(this) || screenId != Workspace.FIRST_SCREEN_ID) {
                 // No need to bind the first screen, as its always bound.
                 mWorkspace.insertNewWorkspaceScreenBeforeEmptyScreen(screenId);
             }
@@ -2356,13 +2363,6 @@ public class Launcher extends StatefulActivity<LauncherState> implements Launche
     }
 
     private View inflateAppWidget(LauncherAppWidgetInfo item) {
-        if (item.hasOptionFlag(LauncherAppWidgetInfo.OPTION_SEARCH_WIDGET)) {
-            item.providerName = QsbContainerView.getSearchComponentName(this);
-            if (item.providerName == null) {
-                getModelWriter().deleteItemFromDatabase(item);
-                return null;
-            }
-        }
         final AppWidgetHostView view;
         if (mIsSafeModeEnabled) {
             view = new PendingAppWidgetHostView(this, item, mIconCache, true);

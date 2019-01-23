121450cef1a47a7691166bf526690f4a106cfe80
Joey <joey@lineageos.org>
2019-01-23 17:49:25 +0100

    Launcher3: Implement hidden & protected apps
    * Launcher3: implement hidden apps
    * Launcher3: Implement protected apps
    
    Squashed with:
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Sat Nov 2 15:23:42 2019 +0100
    
        Switch to BiometricPrompt
    
        Change-Id: Id6700c81aadb64434aecf1e54ae8b0f01d8143fb
    
    Author: Joey <joey@lineageos.org>
    Date:   Fri Feb 7 22:18:51 2020 +0100
    
        Trebuchet: fix protected apps behaviour
    
        If no secure lockscreen is set we should not show
        the app list
    
        Also make icons' touchable area larger
    
        Change-Id: Id398e98cb4d8fdae169aae194211b98e6441b4cf
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Joey <joey@lineageos.org>
    Date:   Sat Apr 4 10:57:37 2020 +0200
    
        Trebuchet: only allow hidden apps when no secure keyguard is set
    
        Change-Id: I670a61ff3fee4d7c7d9623ce8810790f1b25224a
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: Joey <joey@lineageos.org>
    Date:   Wed Aug 5 13:23:03 2020 +0200
    
        Launcher3: improve protected app names style
    
        Change-Id: I09e9995f0eb34de23353166e302481d064fb3058
        Signed-off-by: Joey <joey@lineageos.org>
    
    Author: programminghoch10 <hoch10@kabelbw.de>
    Date:   Mon Apr 18 12:39:22 2022 +0200
    
        Trebuchet: adapt hidden and protected apps for S
    
        * Specifying the AppFilter class in the config.xml does not work anymore
        * Instead we just instantiate our own HiddenAppsFilter class in the LauncherModel
    
        Change-Id: I2a6791083261a580327d99dd1e93eaa019de5bc8
    
    Signed-off-by: Joey <joey@lineageos.org>
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>
    Change-Id: I10c35407820373a1d5f84b237ac449398e076dcd

--- src/com/android/launcher3/settings/SettingsActivity.java
+++ src/com/android/launcher3/settings/SettingsActivity.java
@@ -51,6 +51,8 @@ import androidx.preference.PreferenceScreen;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.launcher3.config.FeatureFlags;
+import com.android.launcher3.lineage.LineageUtils;
+import com.android.launcher3.lineage.trust.TrustAppsActivity;
 import com.android.launcher3.customization.IconDatabase;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.icons.pack.IconPackSettingsActivity;
@@ -96,6 +98,8 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
     static final String EXTRA_FRAGMENT = ":settings:fragment";
     @VisibleForTesting
     static final String EXTRA_FRAGMENT_ARGS = ":settings:fragment_args";
+
+    public static final String KEY_TRUST_APPS = "pref_trust_apps";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -337,6 +341,16 @@ public class SettingsActivity extends CollapsingToolbarBaseActivity
                     return true;
                 case IconDatabase.KEY_ICON_PACK:
                     setupIconPackPreference(preference);
+                    return true;
+                case KEY_TRUST_APPS:
+                    preference.setOnPreferenceClickListener(p -> {
+                        LineageUtils.showLockScreen(getActivity(),
+                                getString(R.string.trust_apps_manager_name), () -> {
+                            Intent intent = new Intent(getActivity(), TrustAppsActivity.class);
+                            startActivity(intent);
+                        });
+                        return true;
+                    });
                     return true;
             }
 

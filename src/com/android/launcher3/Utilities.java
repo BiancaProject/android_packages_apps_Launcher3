71b1232371dc410a811eb9e4eb683592892ab514
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-11 09:01:52 +0800

    Launcher3: Expose force monochrome themed icon flag as a user preference
    Change-Id: I81a58671ca5811ea18fb51c7689a0097eb103e87
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -165,6 +165,7 @@ public final class Utilities {
     public static final String LENS_URI = "google://lens";
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
     public static final String KEY_DOCK_MUSIC_SEARCH = "pref_dock_music_search";
+    public static final String KEY_FORCE_MONOCHROME_ICONS = "pref_forced_monochrome_icons";
 
     /**
      * Returns true if theme is dark.
@@ -867,5 +868,10 @@ public final class Utilities {
     public static boolean isMusicSearchEnabled(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_DOCK_MUSIC_SEARCH, false);
+    }
+       
+    public static boolean enableMonoChromeThemedIcons(Context context) {
+    	SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+    	return prefs.getBoolean(KEY_FORCE_MONOCHROME_ICONS, false);
     }
 }
